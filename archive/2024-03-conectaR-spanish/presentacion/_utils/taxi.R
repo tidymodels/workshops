4e5322eb2d578367c24a7847b8892b816325b79c
Edgar Ruiz <edgararuiz@gmail.com>
2024-02-27 17:09:48 -0600

    Adds translated taxi dataset, and script to translate it

---
+++ archive/2024-03-conectaR-spanish/presentacion/_utils/taxi.R
@@ -0,0 +1,46 @@
+library(tidymodels)
+library(tidyverse)
+
+
+taxi <- taxi %>%
+  rename(
+    propina = tip,
+    distancia = distance,
+    compania = company,
+    dia = dow,
+    mes = month,
+    hora = hour
+  ) %>%
+  mutate(
+    propina = case_when(
+      propina == "yes" ~ "si",
+      TRUE ~ propina
+    ),
+    local = case_when(
+      local == "yes" ~ "si",
+      TRUE ~ local
+    ),
+    compania = case_when(
+      compania == "other" ~ "otra",
+      TRUE ~ compania
+    ),
+    mes = case_when(
+      mes == "Apr" ~ "Abr",
+      mes == "Jan" ~ "Ene",
+      TRUE ~ mes
+    ),
+    dia = case_when(
+      dia == "Mon" ~ "Lun",
+      dia == "Tue" ~ "Mar",
+      dia == "Wed" ~ "Mie",
+      dia == "Thu" ~ "Jue",
+      dia == "Fri" ~ "Vie",
+      dia == "Sat" ~ "Sab",
+      dia == "Sun" ~ "Dom",
+      TRUE ~ dia
+    )
+  )
+
+saveRDS(taxi, here::here("archive/2024-03-conectaR-spanish/taxi.rds"))
+
+rm(taxi)
