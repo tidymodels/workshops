d0051d0f0599bf0020da2a7f46227283e034f40c
Edgar Ruiz <edgararuiz@gmail.com>
2024-02-29 17:23:21 -0600

    Renames 03, makes render script only run the first 3 docs

--- archive/2024-03-conectaR-spanish/presentacion/_utils/docs.R
+++ archive/2024-03-conectaR-spanish/presentacion/_utils/docs.R
@@ -7,12 +7,25 @@ qmds <- dir_ls(source_dir, glob = "*.qmd")
 
 docs <- path(source_dir, "docs")
 
+if(dir_exists(docs)) dir_delete(docs)
 dir_create(docs)
 
-map(qmds,~{
+map(qmds[1:3],~{
   system2("quarto", args = c("render", "input" = .x, paste0("--output-dir=",  docs)))
 })
 
+system2("touch", path(docs, "_site.yml"))
+
 deploy_site <- function() {
-  rsconnect::deploySite(siteDir = docs, siteName = "Aprendiendo Tidymodels")
+  rsconnect::deployApp(
+    appDir = docs
+    )
 }
+
+#deploy_site()
+# https://colorado.posit.co/rsc/conectar2024/intro-01-introduccion.html#/title-slide
+# https://colorado.posit.co/rsc/conectar2024/intro-02-presupuesto-de-datos.html#/title-slide
+# https://colorado.posit.co/rsc/conectar2024/intro-03-what-makes-a-model.html#/title-slide
+
+
+
