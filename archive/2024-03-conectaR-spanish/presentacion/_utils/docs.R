89db480f8f5b8abf43bff14104aa3e664f43875b
Edgar Ruiz <edgararuiz@gmail.com>
2024-02-29 18:11:02 -0600

    Minor fixes to 02 and 03, starts translating code in 04, updates docs.R

--- archive/2024-03-conectaR-spanish/presentacion/_utils/docs.R
+++ archive/2024-03-conectaR-spanish/presentacion/_utils/docs.R
@@ -10,7 +10,7 @@ docs <- path(source_dir, "docs")
 if(dir_exists(docs)) dir_delete(docs)
 dir_create(docs)
 
-map(qmds[1:3],~{
+map(qmds[1:4],~{
   system2("quarto", args = c("render", "input" = .x, paste0("--output-dir=",  docs)))
 })
 
@@ -25,7 +25,7 @@ deploy_site <- function() {
 #deploy_site()
 # https://colorado.posit.co/rsc/conectar2024/intro-01-introduccion.html#/title-slide
 # https://colorado.posit.co/rsc/conectar2024/intro-02-presupuesto-de-datos.html#/title-slide
-# https://colorado.posit.co/rsc/conectar2024/intro-03-what-makes-a-model.html#/title-slide
+# https://colorado.posit.co/rsc/conectar2024/intro-03-partes-del-modelo.html#/title-slide
 
 
 
