c88b980d944c40a7fb9a6208df09356a0e2c7bc9
Edgar Ruiz <edgararuiz@gmail.com>
2024-02-29 18:33:28 -0600

    Renames 04, improvements to docs.R

--- archive/2024-03-conectaR-spanish/presentacion/_utils/docs.R
+++ archive/2024-03-conectaR-spanish/presentacion/_utils/docs.R
@@ -10,11 +10,16 @@ docs <- path(source_dir, "docs")
 if(dir_exists(docs)) dir_delete(docs)
 dir_create(docs)
 
+quarto_yml <- path(source_dir, "_quarto.yml")
+system2("touch", quarto_yml)
+
 map(qmds[1:4],~{
   system2("quarto", args = c("render", "input" = .x, paste0("--output-dir=",  docs)))
 })
 
 system2("touch", path(docs, "_site.yml"))
+
+file_delete(quarto_yml)
 
 deploy_site <- function() {
   rsconnect::deployApp(
@@ -26,6 +31,6 @@ deploy_site <- function() {
 # https://colorado.posit.co/rsc/conectar2024/intro-01-introduccion.html#/title-slide
 # https://colorado.posit.co/rsc/conectar2024/intro-02-presupuesto-de-datos.html#/title-slide
 # https://colorado.posit.co/rsc/conectar2024/intro-03-partes-del-modelo.html#/title-slide
-
+# https://colorado.posit.co/rsc/conectar2024/intro-04-evaluar-modelos.html#/title-slide
 
 
